87c67b593afe4e0cd5b25b53298b80e92968eef4
JoaoPedroFiguraBurkot <joao.burkot@escola.pr.gov.br>
2024-10-22 13:20:22 -0300

    Add files via upload

---
+++ projeto alura 3 tri/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Literatura',
+    'Quem foi o autor de O Pequeno Príncipe?',
+    'Antoine de Saint-Exupéry'
+)
+
+criaCartao(
+    'Biologia',
+    'Quantos ossos tem o corpo Humano?',
+    '206 ossos considerados constantes, mas o número pode variar'
+)
+
+criaCartao(
+    'História',
+    'Quando ocorreu a independencia do Brasil?',
+    '7 de setembro de 1822'
+)
+
+criaCartao(
+    'Filosofia',
+    'qual significado da palavra  filosofia?',
+    'é de origem grega, e significa “amor à sabedoria”)'
+)
